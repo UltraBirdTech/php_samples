649d6b9757020e483e8fd0669e2d0e9a2eb63006
UltraBirdTech <ultrabirdrepti@gmail.com>
2017-12-04 20:07:41 +0900

    change file names

--- php_elementary/chapter3_Q3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = '10';
-
-if ($data == 10) {
-  print '==は型の比較までしないからtrue';
-} else {
-  print 'falseではない';
-}
-  
--- php_elementary/chapter3_Q5.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if('5member' < 44){
-  print '44 is bigger';
-} else {
-  print '5member is begger';
-}
