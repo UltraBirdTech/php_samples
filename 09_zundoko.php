3a9e0f2e1caf039f88fd7615f24c8c6d09bac2bd
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-04-08 17:28:48 +0900

    fix comment

--- 09_zundoko.php
+++ 09_zundoko.php
@@ -3,19 +3,16 @@
   CONST ZUN_DOKO_STRING = 'ズンズンズンズンドコ';
   CONST ZUN_DOKO_CHECK_COUNT = 5;
   $array = []; // 格納する配列を定義
-  // loop
   while( true ) {
     $char = ZUN_DOKO[array_rand(ZUN_DOKO, 1)];
     print_r($char);
     array_push($array, $char);
     // array の長さが5の場合のみ確認する
     if ((count($array)) === ZUN_DOKO_CHECK_COUNT ) {
-      // check
       if ( implode('', $array) === ZUN_DOKO_STRING ) {
         print_r("KI ☆ YO ☆ SHI !!\n");
         break;
       }
-      // shift
-      array_shift($array); 
+      array_shift($array); // shift
     } 
   }
