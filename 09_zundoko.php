7046ba454d1e5facda1729819dd30a1c202bcc0a
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-04-08 17:25:47 +0900

    文字列を直書きしていた部分を定数にする

--- 09_zundoko.php
+++ 09_zundoko.php
@@ -1,15 +1,17 @@
 <?php
-  $array = [];
   CONST ZUN_DOKO = array('ズン', 'ドコ');
+  CONST ZUN_DOKO_STRING = 'ズンズンズンズンドコ';
+  CONST ZUN_DOKO_CHECK_COUNT = 5;
+  $array = []; // 格納する配列を定義
   // loop
   while( true ) {
     $char = ZUN_DOKO[array_rand(ZUN_DOKO, 1)];
     print_r($char);
     array_push($array, $char);
     // array の長さが5の場合のみ確認する
-    if ((count($array)) === 5) {
+    if ((count($array)) === ZUN_DOKO_CHECK_COUNT ) {
       // check
-      if ( implode('', $array) === 'ズンズンズンズンドコ' ) { 
+      if ( implode('', $array) === ZUN_DOKO_STRING ) { 
         print_r("KI ☆ YO ☆ SHI !!\n");
         break;
       }
