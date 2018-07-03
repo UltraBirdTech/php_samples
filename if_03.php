cab181b0f475a7a9a24c1a522393878a6b24b24c
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-07-03 20:28:52 +0900

    add if_03.php

---
+++ if_03.php
@@ -0,0 +1,17 @@
+<?php
+
+$target = '0';
+
+if($target == !isValid($target)) {
+  echo 'false'; // エラー処理（エラー文言入れて画面に表示)
+  return 'something';
+}
+
+// ビジネスロジックが続く
+
+/**
+* 何かしらの値（メールアドレスや電話番号）が正当か確認するメソッド 
+*/
+function isValid($target) {
+  return false; // 実際は$targetの正当性を検証してtrue or false
+}
