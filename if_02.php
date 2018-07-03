78c41b25bb08d5f2218ed7c516d7c3c703463211
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-07-03 20:03:16 +0900

    add if_02.php

---
+++ if_02.php
@@ -0,0 +1,17 @@
+<?php
+
+$target = 'valid target';
+
+if(!isValid($target)) {
+  echo 'false'; // エラー処理（エラー文言入れて画面に表示)
+  return 'something';
+}
+
+// ビジネスロジックが続く
+
+/**
+* 何かしらの値（メールアドレスや電話番号）値が正当か確認するメソッド 
+*/
+function isValid($target) {
+  return false; // 実際は$targetの正当性を検証してtrue or false
+}
