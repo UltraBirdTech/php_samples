04e15b43b09e9d1538be051bf1b4983be4e42fdf
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-07-03 19:50:08 +0900

    edit logic

--- if_01.php
+++ if_01.php
@@ -3,14 +3,15 @@
 $target = 'valid target';
 
 if($target == !isValid($target)) {
-  echo 'true'; // 実際は業務ロジック
-} else {
-  echo 'false'; // 実際はエラー処理（エラー文言入れて画面に表示)
+  echo 'false'; // エラー処理（エラー文言入れて画面に表示)
+  return 'something';
 }
+
+// ビジネスロジックが続く
 
 /**
 * 何かしらの値（メールアドレスや電話番号）の値が正当か確認するメソッド 
 */
 function isValid($target) {
-  return true; // 実際は$targetの正当性を検証してtrue or false
+  return false; // 実際は$targetの正当性を検証してtrue or false
 }
