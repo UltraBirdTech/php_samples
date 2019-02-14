2948cfc52ab5faec47abd8d2bcab1910a2b06247
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-02-15 07:05:13 +0900

    add comments.

--- specialchars.php
+++ specialchars.php
@@ -1,4 +1,7 @@
 <?php
 
 var_dump("<script>alert('XSS!!!!')</script>");
+# string(33) "<script>alert('XSS!!!!')</script>"
+
 var_dump(htmlspecialchars("<script>alert('XSS!!!!')</script>"));
+# string(45) "&lt;script&gt;alert('XSS!!!!')&lt;/script&gt;"
